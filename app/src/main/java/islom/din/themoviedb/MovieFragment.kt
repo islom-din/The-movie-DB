b02d5893d296e59f119d7341b54c37da95f7c95b
Islom Nuridinov <islom.din.nur@gmail.com>
2021-03-30 01:23:17 +0500

    add fragment for movie details

--- app/src/main/java/islom/din/themoviedb/MovieFragment.kt
+++ app/src/main/java/islom/din/themoviedb/MovieFragment.kt
@@ -1,11 +1,15 @@
 package islom.din.themoviedb
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_movie.*
 import kotlinx.android.synthetic.main.fragment_movie.view.*
+
 
 class MovieFragment : Fragment(R.layout.fragment_movie) {
 
@@ -22,13 +26,37 @@ class MovieFragment : Fragment(R.layout.fragment_movie) {
 
         movieViewModel.listOfMoviesLiveData.observe(requireActivity()) {
             val movie = it[moviePosition!!]
-            view.text.text = "${movie.name}\n${movie.overview}"
-            setText(movie)
+            initMovieData(view, movie)
+        }
+
+        view.back_button.setOnClickListener {
+            findNavController().popBackStack()
         }
     }
 
-    private fun setText(movie: Result) {
+    @SuppressLint("CheckResult", "SetTextI18n")
+    private fun initMovieData(view: View, movie: Result) {
+        Glide.with(view)
+            .load("https://image.tmdb.org/t/p/w500/${movie.backdrop_path}")
+            .centerCrop()
+            .into(view.background_image)
 
+        Glide.with(view)
+            .load("https://image.tmdb.org/t/p/w500/${movie.poster_path}")
+            .centerCrop()
+            .into(view.image)
+
+        view.name.text = movie.original_name
+        view.year.text = movie.first_air_date
+        view.popularity.text = "Popularity: ${movie.popularity}"
+        view.vote_count.text = "Vote count: ${movie.vote_count}"
+        var resultText = "Countries: "
+        for(item in movie.origin_country!!) {
+            resultText += "$item "
+        }
+        view.vote.text = movie.vote_average.toString()
+        view.overview.text = movie.overview
     }
+
 
 }
